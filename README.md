5a94c2fca66366a458b39991eb7b1ae8c0a87db2
Batuhan <batuhan108668@gmail.com>
2021-07-18 17:56:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<a href="https://voiddevs.org/dc" target="_blank"><img src="https://cdn.discordapp.com/attachments/789434108614541394/827535985562091540/vdevs.png" alt="Void Devs"></a>
---
+++ voidserver-main.js
@@ -0,0 +1,72 @@
+//------------- DEFINITIONS
+const Discord = require("discord.js");
+const fs = require('fs');
+const client = new Discord.Client({ fetchAllMembers: true, disableMentions: 'here', disableMentions: 'everyone' });
+//------------- DEFINITIONS
+
+//------------- CONFIG
+global.config = {
+  "token": "ODU4MDk5Nzg3MDc2NDY4NzU3.YNZNsw.7z31XCdmPuMdoXEW4clrnkQ39g4",
+  "clientid": "858099787076468757",
+  "secret": "CVPfowk_rmg1k_zH1Fm7lWl0230DSZLy",
+  "callback": "",
+  "site": ""
+}
+//------------- CONFIG
+
+
+//-------------Events---------------\\
+
+require('events').EventEmitter.prototype._maxListeners = 100;
+
+// Command Loader
+client.komutlar = new Discord.Collection();
+client.aliases = new Discord.Collection();  
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.error(err);
+  console.log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+   if(!f.endsWith('.js')) return 
+    let props = require(`./commands/${f}`);
+   if(!props.help) return
+    console.log(`Yüklenen komut: { ${props.help.name} }`);
+    client.komutlar.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+          global.commands = files;
+    });
+  });
+});
+
+// Prefix Avaible
+client.on('message', async message => {
+let p = "s-"
+let client = message.client;
+if (message.author.bot) return;
+if (!message.content.startsWith(p)) return;
+let command = message.content.split(" ")[0].slice(p.length);
+let params = message.content.split(" ").slice(1);
+let cmd
+if (client.komutlar.has(command)) {
+cmd = client.komutlar.get(command);
+} else if (client.aliases.has(command)) {
+cmd = client.komutlar.get(client.aliases.get(command));
+}
+if (cmd) {
+}
+cmd.run(client, message, params, p);
+})
+
+
+//------------- WEBSITE
+require("./voidserver.js")(client);
+//------------- WEBSITE
+
+//------------- CLIENT
+client.login(global.config.token);
+client.on("ready", () => {
+  client.user.setPresence({ status: "dnd" })
+  client.user.setActivity("Screw Development", { type: "WATCHING" });
+  console.log("screwdevelopment.org is online.");
+})
+//------------- CLIENT
